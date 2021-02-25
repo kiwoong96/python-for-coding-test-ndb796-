62a79992e3e63652cad4ecf25a5ef187bf15dd04
kiwoong96 <justlikethat8778@gmail.com>
2021-02-26 00:00:28 +0900

    2020.02.25

--- 알고리즘 기출문제/최단 경로/39_화성 탐사/solution.py
+++ 알고리즘 기출문제/최단 경로/39_화성 탐사/solution.py
@@ -1,7 +1,10 @@
+import heapq
 import sys
 input = sys.stdin.readline
 INF = int(1e9)
 T = int(input())
+
+dxy = [[-1,0],[1,0],[0,1],[0,-1]]
 
 for _ in range(T):
     N = int(input())
@@ -10,18 +13,47 @@ for _ in range(T):
     for _ in range(N):
         board.append(list(map(int,input().split())))
 
+    distance = [[INF for _ in range(N)] for _ in range(N)]
+    distance[0][0] = board[0][0]
 
-    dp = [[INF for _ in range(N+2)]for _ in range(N+2)]
+    q = []
+    heapq.heappush(q,(board[0][0],0,0))
 
-    for i in range(1,N+1):
-        for j in range(1,N+1):
-            dp[i][j] = board[i-1][j-1]
+    while q:
+        dist, x, y = heapq.heappop(q)
 
-    for i in range(1,N+1):
-        for j in range(1,N+1):
-            if i == 1 and j == 1: continue
-            dp[i][j] = min(dp[i-1][j] + dp[i][j], dp[i][j-1] + dp[i][j])
 
-    for i in dp:
-        print(i)
+        if distance[x][y] < dist:
+            continue
+
+        for dt in dxy:
+            tmp_x, tmp_y = x+dt[0], y+dt[1]
+            if 0<=tmp_x<N and 0<=tmp_y<N:
+                if distance[tmp_x][tmp_y] > dist+board[tmp_x][tmp_y]:
+                    distance[tmp_x][tmp_y] = dist + board[tmp_x][tmp_y]
+                    heapq.heappush(q,(distance[tmp_x][tmp_y],tmp_x,tmp_y))
+    print(distance[N - 1][N - 1])
+
+
+"""
+3
+3
+5 5 4
+3 9 1
+3 2 7
+5
+3 7 2 0 1
+2 8 0 9 1
+1 2 1 8 1
+9 8 9 2 0
+3 6 5 1 5
+7
+9 0 5 1 1 5 3
+4 1 2 1 6 5 3
+0 7 6 1 6 8 5
+1 1 7 8 3 2 3
+9 4 0 7 6 4 1
+5 8 3 2 4 8 3
+7 4 8 4 8 3 4
+"""
 
